5d2f65cf7baf7aeb797c9fcc298c6ddcb56fb889
Danylo <Danylo.Brudnyi.RI.2023@lpnu.ua>
2023-11-08 15:26:09 +0200

    my first commit

---
+++ PR-07.1іт.cpp
@@ -0,0 +1,115 @@
+﻿// Lab_7_1.cpp
+// < Брудного Данила >
+// Лабораторна робота № 7.1.
+// Пошук заданих елементів та впорядкування рядків / стовпчиків матриці.
+// Варіант 3
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int** T, const int rowCount, const int colCount, const int Low,
+	const int High);
+void Print(int** T, const int rowCount, const int colCount);
+void Sort(int** T, const int rowCount, const int colCount);
+void Change(int** T, const int row1, const int row2, const int colCount);
+void Calc(int** T, const int rowCount, const int colCount, int& S, int& k);
+
+
+void Create(int** T, const int rowCount, const int colCount, const int Low,
+	const int High)
+{
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			T[i][j] = Low + rand() % (High - Low + 1);
+}
+void Print(int** T, const int rowCount, const int colCount)
+{
+	cout << endl;
+	for (int i = 0; i < rowCount; i++)
+	{
+		for (int j = 0; j < colCount; j++)
+			cout << setw(4) << T[i][j];
+		cout << endl;
+	}
+	cout << endl;
+}
+void Sort(int** T, const int rowCount, const int colCount)
+{
+	for (int i0 = 0; i0 < rowCount - 1; i0++)
+		for (int i1 = 0; i1 < rowCount - i0 - 1; i1++)
+			if ((T[i1][0] > T[i1 + 1][0])
+				||
+				(T[i1][0] == T[i1 + 1][0] &&
+					
+					T[i1][1] < T[i1 + 1][1])
+				||
+				(T[i1][0] == T[i1 + 1][0] &&
+					T[i1][1] == T[i1 + 1][1] &&
+					T[i1][2] > T[i1 + 1][2]))
+				Change(T, i1, i1 + 1, colCount);
+}
+void Change(int** T, const int row1, const int row2, const int colCount)
+{
+	int tmp;
+	for (int j = 0; j < colCount; j++)
+	{
+		tmp = T[row1][j];
+		T[row1][j] = T[row2][j];
+		T[row2][j] = tmp;
+	}
+}
+void Calc(int** T, const int rowCount, const int colCount, int& S, int& k)
+{
+	S = 0;
+	k = 0;
+
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			if (T[i][j] % 2 == 0 && !(T[i][j] % 5 == 0))
+			{
+				S += T[i][j];
+				k++;
+				T[i][j] = 0;
+			}
+}
+
+int Sum(int** T, const int rowCount, const int colCount)
+{
+	int totalSum = 0;
+	for (int i = 0; i < rowCount; i++)
+	{
+		for (int j = 0; j < colCount; j++)
+		{
+			totalSum += T[i][j];
+		}
+	}
+	return totalSum;
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int Low = -42;
+	int High = 51;
+	int rowCount = 7;
+	int colCount = 6;
+	int** T = new int* [rowCount];
+	for (int i = 0; i < rowCount; i++)
+		T[i] = new int[colCount];
+	Create(T, rowCount, colCount, Low, High);
+	Print(T, rowCount, colCount);
+	Sort(T, rowCount, colCount);
+	int S = 0;
+	int k = 0;
+	Calc(T, rowCount, colCount, S, k);
+	cout << "S = " << S << endl;
+	cout << "k = " << k << endl;
+	 
+	for (int i = 0; i < rowCount; i++)
+		delete[] T[i];
+	delete[] T;
+	return 0;
+}
---
+++ PR-7.1-a/PR-7.1-a.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+int FindElement(int** T, const int rowCount, const int colCount, int target)
+{
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < colCount; j++) {
+            if (T[i][j] == target) {
+                // Знайдено шуканий елемент, повертаємо його індекс
+                return i * colCount + j;
+            }
+        }
+    }
+    // Якщо елемент не знайдено, повертаємо -1
+    return -1;
+}
+
+void Create(int** T, const int rowCount, const int colCount, const int Low, const int High)
+{
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                T[i][j] = Low + rand() % (High - Low + 1);
+            }
+        }
+    }
+
+
+void Print(int** T, const int rowCount, const int colCount)
+{
+        cout << endl;
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                cout << setw(4) << T[i][j];
+            }
+            cout << endl;
+        }
+        cout << endl;
+}
+
+
+
+void Sort(int** T, const int rowCount, const int colCount)
+{
+        for (int i0 = 0; i0 < rowCount - 1; i0++)
+            for (int i1 = 0; i1 < rowCount - i0 - 1; i1++)
+                if ((T[i1][0] > T[i1 + 1][0])
+                    ||
+                    (T[i1][0] == T[i1 + 1][0] &&
+
+                        T[i1][1] < T[i1 + 1][1])
+                    ||
+                    (T[i1][0] == T[i1 + 1][0] &&
+                        T[i1][1] == T[i1 + 1][1] &&
+                        T[i1][2] > T[i1 + 1][2]))
+                    Change(T, i1, i1 + 1, colCount);
+}
+
+
+void Change(int** T, const int row1, const int row2, const int colCount)
+{
+    int tmp;
+    for (int j = 0; j < colCount; j++)
+    {
+        tmp = T[row1][j];
+        T[row1][j] = T[row2][j];
+        T[row2][j] = tmp;
+    }
+}
+
+
+
+void Calc(int** T, const int rowCount, const int colCount, int& S, int& k)
+{
+    S = 0;
+    k = 0;
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < colCount; j++) {
+            if (T[i][j] % 2 == 0 && !(T[i][j] % 5 == 0)) {
+                S += T[i][j];
+                k++;
+                T[i][j] = 0;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int Low = -42;
+    int High = 51;
+    int rowCount = 7;
+    int colCount = 6;
+    int** T = new int* [rowCount];
+    for (int i = 0; i < rowCount; i++)
+        T[i] = new int[colCount];
+    Create(T, rowCount, colCount, Low, High);
+    Print(T, rowCount, colCount);
+    Sort(T, rowCount, colCount);
+    int S = 0;
+    int k = 0;
+    Calc(T, rowCount, colCount, S, k);
+
+    // Використання функції FindElement для пошуку конкретного елемента
+    int target = 5; // Замініть на ваш пошуковий елемент
+    int result = FindElement(T, rowCount, colCount, target);
+    if (result != -1) {
+        cout << "Found element " << target << " at index " << result << endl;
+    }
+    else {
+        cout << "Element " << target << " not found." << endl;
+    }
+
+    cout << "S = " << S << endl;
+    cout << "k = " << k << endl;
+    Print(T, rowCount, colCount);
+    for (int i = 0; i < rowCount; i++)
+        delete[] T[i];
+    delete[] T;
+    return 0;
+}
